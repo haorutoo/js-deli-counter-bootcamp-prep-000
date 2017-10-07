7c7c845f4288b3f48952dea28f7b2203cf4c024d
haorutoo <haorutoo@gmail.com>
2017-10-07 06:09:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,22 @@ function nowServing(katzDeliLine){
   return katzDeli
 
 }
+
+function currentLine(line){
+  if (line.length == 0){
+    return "The line is currently empty."
+  }    
+  else {
+    var string //= "The line is currently: " 
+    for (i = 0, i < line.length, i++){
+      var counter = i + 1
+      if (i = line.length - 1) {
+        string = counter + ". " + line[i]                 
+      }
+      else {
+        string = counter + ". " + line[i] + ", "        
+      }
+    }
+    return 
+  }
+}
