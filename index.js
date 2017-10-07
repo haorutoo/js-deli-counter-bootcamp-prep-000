361fe27fa6373fc8553d5590fc099dfadf611fe2
haorutoo <haorutoo@gmail.com>
2017-10-07 06:13:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function currentLine(line){
     var string = "" //= "The line is currently: "
     for (i = 0, i < line.length, i++){
       var counter = i + 1
-      if (i = line.length - 1) {
+      if (i == line.length - 1) {
         string = counter + ". " + line[i]
       }
       else {
