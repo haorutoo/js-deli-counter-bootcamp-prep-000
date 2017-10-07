56c0d21cacd1e0dd8035d5952aaf8d2f04bbf9f4
haorutoo <haorutoo@gmail.com>
2017-10-07 05:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function takeANumber(katzDeliLine,name){
   var counter = katzDeliLine.length
   return "Welcome, " + name + ". You are number " + counter + " in line."
 
+  katzDeliLine.push(name)
   katzDeli.push(name)
-
+  
 }
