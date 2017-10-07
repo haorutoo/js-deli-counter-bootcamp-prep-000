78bca8e8b3b43d995fb219ca6572bc9da32d5ec2
haorutoo <haorutoo@gmail.com>
2017-10-07 06:23:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function nowServing(katzDeliLine){
     return "There is nobody waiting to be served!"
     }
   else {
-    var string = "Currently serving " + katzDeliLine[0] + "." 
+    var string = "Currently serving " + katzDeliLine[0] + "."
     katzDeliLine.shift()
     katzDeli.shift()
-    return string  
+    return string
     return katzDeliLine
     return katzDeli
 
